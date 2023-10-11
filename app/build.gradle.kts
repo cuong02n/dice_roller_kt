7543d519b823a3bbdc33197321465076df4d5aa6
cuong02n <cuong02n@gmail.com>
2023-10-11 22:50:51 +0700

    complete

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.cuong02n.dice_roller"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.cuong02n.dice_roller"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -58,6 +58,7 @@ dependencies {
     implementation("androidx.compose.material3:material3")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/cuong02n/dice_roller/MainActivity.kt
+++ app/src/main/java/com/cuong02n/dice_roller/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.cuong02n.dice_roller
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -17,5 +21,20 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val button : Button = findViewById(R.id.rollButton)
+
+        button.setOnClickListener {
+            val dice = (1..6).random()
+            val img: ImageView = findViewById(R.id.imageView)
+            when (dice) {
+                1 -> img.setImageResource(R.drawable.dice_1)
+                2 -> img.setImageResource(R.drawable.dice_2)
+                3 -> img.setImageResource(R.drawable.dice_3)
+                4 -> img.setImageResource(R.drawable.dice_4)
+                5 -> img.setImageResource(R.drawable.dice_5)
+                6 -> img.setImageResource(R.drawable.dice_6)
+            }
+            Toast.makeText(this, dice.toString(), Toast.LENGTH_SHORT).show()
+        }
     }
 }
